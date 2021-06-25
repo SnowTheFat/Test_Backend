fa7bd06b9081f93301e6589d3a6be6520a98f2a3
SnowTheFat <86468559+SnowTheFat@users.noreply.github.com>
2021-06-25 16:16:08 +0700

    login file

--- h.php
+++ h.php
@@ -1,28 +1,17 @@
-<?php session_start(); 
-include('condb.php');
-
-  $user_id = $_SESSION['user_id'];
-  $a_name = $_SESSION['a_name'];
- 	if($user_id==''){
-    Header("Location: ../logout.php");  
-  }  
-?>
-<meta charset="utf-8">
-  <meta http-equiv="X-UA-Compatible" content="IE=edge">
-  <title>Sunbuck Coffee</title>
-  <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
-
-  <!--start data table-->
-<link rel="stylesheet" type="text/css" href="//cdn.datatables.net/1.10.11/css/jquery.dataTables.min.css">
-<script type="text/javascript" language="javascript" src="//code.jquery.com/jquery-1.12.0.min.js"></script>
-<script type="text/javascript" src="//cdn.datatables.net/1.10.11/js/jquery.dataTables.min.js">
-</script>
-<script>
-$(document).ready(function() {
-$('#example').DataTable( {
-"aaSorting" :[[0,'desc']],
-//"lengthMenu":[[20,50, 100, -1], [20,50, 100,"All"]]
-});
-} );
-</script>
-<!-- end data table -->
+<?php include('backend/condb.php');?>
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <!-- Bootstrap -->
+    <!-- Latest compiled and minified CSS -->
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" integrity="sha384-1q8mTJOASx8j1Au+a5WDVnPi2lkFfwwEAa8hDDdjZlpLegxhjVME1fgjWPGmkzs7" crossorigin="anonymous">
+    <!-- Optional theme -->
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap-theme.min.css" integrity="sha384-fLW2N01lMqjakBkx3l/M9EahuwpSfeNvV63J5ezn3uZzapT0u7EYsXMjQV+0En5r" crossorigin="anonymous">
+    <title>Sunbuck Coffee</title>
+    <link href="css/bootstrap.min.css" rel="stylesheet">
+    <link href="css/style.css" rel="stylesheet">
+  </head>
+  <body>
--- index.php
+++ index.php
@@ -1,27 +1,41 @@
-<!DOCTYPE html>
-<html>
-
-<head>
-  <?php include('h.php'); ?>
-
-  <head>
-
-  <body>
-    <div class="container">
-      <?php include('navbar.php'); ?>
-      <p></p>
-      <div class="row">
-        <div class="col-md-2">
-          สวัสดี คุณ <?php echo $a_name; ?>
-          <!-- Left side column. contains the logo and sidebar -->
-          <?php include('menu_left.php'); ?>
+<?php session_start();?>
+<?php
+include('h.php');
+?>
+<style type="text/css">
+#btn{
+width:100%;
+}
+</style>
+<div class="container" style="padding-top:100px">
+  <div class="row">
+    <div class="col-md-4"></div>
+    <div class="col-md-4" style="background-color:#D6EAF8">
+      <h3 align="center">
+      <span class="glyphicon glyphicon-lock"> </span>
+      Login </h3>
+      <form  name="formlogin" action="checklogin.php" method="POST" id="login" class="form-horizontal">
+        <div class="form-group">
+          <div class="col-sm-12">
+            <input type="text"  name="a_user" class="form-control" required placeholder="Username" />
+          </div>
         </div>
-        <div class="col-md-10">
-          <?php include('show_product.php'); ?>
+        <div class="form-group">
+          <div class="col-sm-12">
+            <input type="password" name="a_pass" class="form-control" required placeholder="Password" />
+          </div>
         </div>
-      </div>
+        <div class="form-group">
+          <div class="col-sm-12">
+            <button type="submit" class="btn btn-success" id="btn">
+            <span class="glyphicon glyphicon-log-in"> </span>
+             Login </button>
+               <label>
+                <input type="checkbox" checked="checked" name="remember"> Remember me
+               </label>
+          </div>
+        </div>
+      </form>
     </div>
-    </div>
-  </body>
-
-</html>
+  </div>
+</div>
